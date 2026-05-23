eb9b3ba01c049373cdca3fca5748e5c4d87cccd1
TathagatGupta98 <janaktathagat@gmail.com>
2026-05-24 00:42:30 +0530

    feat: Separate Notifications model key realation to User and a task. Bolean of read/unread used.

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -79,3 +79,13 @@ class Attachment(models.Model):
 
     def __str__(self):
         return f"Attachment for {self.task.title}"
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications")
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="notifications")
+    message = models.CharField(max_length=255)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.user.username}: {self.message}"
--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
-from .models import Project, Task, Tag, Comment, Attachment
+from .models import Project, Task, Tag, Comment, Attachment, Notification
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = [
+            "id",
+            "name"
+        ]
 
 class CommentSerializer(serializers.ModelSerializer):
     author_username = serializers.ReadOnlyField(source="author.username")
@@ -26,6 +34,20 @@ class AttachmentSerializer(serializers.ModelSerializer):
             "created_at",
         ]
 
+class NotificationSerializer(serializers.ModelSerializer):
+    user_username = serializers.ReadOnlyField(source="user.username")
+
+    class Meta:
+        model = Notification
+        fields = [
+            "id",
+            "user",
+            "user_username",
+            "task",
+            "message",
+            "is_read",
+            "created_at",
+        ]
 
 class TaskSerializer(serializers.ModelSerializer):
     assigned_to_username = serializers.ReadOnlyField(source="assigned_to.username")
@@ -57,6 +79,7 @@ class TaskSerializer(serializers.ModelSerializer):
             "tags",
             "comments",
             "attachments",
+            "notifications",
         ]
 
 
